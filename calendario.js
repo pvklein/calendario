5d08b9618c50c4ae8f76cdda3b70c8829023b982
unknown <javi.mph@gmail.com>
2019-09-19 19:18:27 +0200

    v.0.0.3

--- calendario.js
+++ calendario.js
@@ -1,45 +1,58 @@
 $( document ).ready(function() {
     
+    //$('#myModal .close').append('<h4 class="modal-title">Modal Header</h4>');
+    //$('#myModal').modal('show');
+    //$('#myModal').modal('show');
     var d = new Date();
     //Para comporbar si diciembre lo hace bien d.setMonth(d.getMonth()-8);
     var day=d.getDay();
     var month=d.getMonth()+1;
     var year=d.getFullYear();
-    var day_week=d.getDay();
+    var day_week=d.getDay();       
 
-    if (month==1)
-    {
-        var lastyear=year-1;
-        var lastmonth=12;
-    }
-    else
-    {
-        var lastyear=year;
-        var lastmonth=month-1;
-    }   
+    var my_last_next=[];
+    my_last_next.length=6;
 
-    //Empezamos dibujando este mes
-    dibuja_dias(month,year);    
-
+    my_last_next=calcula_anterior_posterio(month,year);
+    
+    //Empezamos dibujando este mes    
+    dibuja_dias(my_last_next);  
+    
     $("#mesizqda").click(function(){
         $("#mesano").empty();
         $("#dias").empty();
-        dibuja_dias(lastmonth,lastyear)
-        //alert("izquierda");
+        my_last_next=calcula_anterior_posterio(my_last_next[0],my_last_next[1]);   
+        dibuja_dias(my_last_next);
+            
     });
+
+    $("#mesdcha").click(function(){
+        $("#mesano").empty();
+        $("#dias").empty();
+        my_last_next=calcula_anterior_posterio(my_last_next[2],my_last_next[3]);
+        dibuja_dias(my_last_next);
+    });
+
+    $(document).on('click', '.fecha', function(e) {
+        console.log(this.id);        
+        Cargar_modal(this.id);        
+      });
 
 });
 
-function dibuja_dias(month,year)
+//Dibuja el mes con el vector que le de exactamente
+function dibuja_dias(my_last_next)
 {
+    console.log(my_last_next);    
     const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
   "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
-];
-    $('#mesano').append('<div class="col"><h1>'+monthNames[month-1]+' '+year+'</h1></div>');
+]
+    $('#mesano').append('<div class="col"><h1>'+monthNames[my_last_next[4]-1]+' '+my_last_next[5]+'</h1></div>');
 
     var array_enmes=[];
-    array_enmes.length=42;
-    array_enmes=array_diasmes(month,year);    
+    array_enmes.length=84;
+    array_enmes=array_diasmes(my_last_next);    
+    //console.log(array_enmes);
 
     var columnas="";
     
@@ -48,15 +61,20 @@ function dibuja_dias(month,year)
         columnas=columnas+'<div class="row">';
         for (i=1;i<8;i++)
         {
-            columnas=columnas+'<div class="col-sm col-1 fecha"><h1>'+array_enmes[j*7+i]+'</h1></div>';
+            columnas=columnas+'<div name="midia" id="'+("0"+array_enmes[j*7+i].toString()).slice(-2)+array_enmes[j*7+i+42].toString()+'" class="col-sm col-1 fecha"><h1>'+array_enmes[j*7+i]+'</h1></br></div>';
         } 
         columnas=columnas+'</div>';
     }
     $('#dias').append(columnas);
+    Dibujar_Titulos(my_last_next[4],my_last_next[5])
 }
 
-function array_diasmes(month,year)
-{      
+function array_diasmes(my_last_next)
+{  
+    var month=my_last_next[4];
+    var year=my_last_next[5];
+    var array_mes=[];
+    array_mes.length=84; 
 
     if (month==1)
     {
@@ -76,30 +94,145 @@ function array_diasmes(month,year)
     if (firstDaymonth==0)
     {
         firstDaymonth=7;
-    }
-    var array_mes=[];
-    array_mes.length=42;
+    }    
 
     for (var i=1;i<firstDaymonth;i++)
     {
         array_mes[i]=days_inlastmonth-firstDaymonth+i+1;
+        array_mes[i+42]=("0"+my_last_next[0].toString()).slice(-2)+my_last_next[1].toString()
    }
 
    var j=1;
-   for (var i=firstDaymonth;i<=days_inmonth+firstDaymonth;i++)
+   for (var i=firstDaymonth;i<days_inmonth+firstDaymonth;i++)
     {
         array_mes[i]=j;
+        array_mes[i+42]=("0"+my_last_next[4].toString()).slice(-2)+my_last_next[5].toString()
         j++;
     }
     j=1;
-    for (var i=days_inmonth+firstDaymonth+1;i<=43;i++)
+    for (var i=days_inmonth+firstDaymonth;i<43;i++)
     {
         array_mes[i]=j;
+        array_mes[i+42]=("0"+my_last_next[2].toString()).slice(-2)+my_last_next[3].toString()
         j++;
     }
-    
+    //console.log(array_mes);
     return array_mes;
     //console.log(firstDaymonth);
     
+}
+
+function calcula_anterior_posterio(month,year)
+{
+    //Defined: [last_month, last_year, next_month, next_year, actual_month, actual_year]
+    var array_anterior_posterior=[];
+    array_anterior_posterior.length=6;
+
+    if (month==1)
+    {
+        array_anterior_posterior[1]=year-1;
+        array_anterior_posterior[3]=year;
+        array_anterior_posterior[0]=12;
+        array_anterior_posterior[2]=month+1;
+    }
+    else
+    {
+        if (month==12)
+        {
+            array_anterior_posterior[1]=year;
+            array_anterior_posterior[3]=year+1;
+            array_anterior_posterior[0]=month-1;
+            array_anterior_posterior[2]=1;
+
+        }
+        else
+        {
+            array_anterior_posterior[1]=year;
+            array_anterior_posterior[3]=year;
+            array_anterior_posterior[0]=month-1;
+            array_anterior_posterior[2]=month+1;
+        }
+    }
+    array_anterior_posterior[4]=month;
+    array_anterior_posterior[5]=year;
+    return array_anterior_posterior;
+
+}
+
+function Dibujar_Titulos(month,year)
+{    
+    var funcion='encontrartitulos';       
+	
+	var parametros = {
+                "select" : funcion,				
+				"month" : month,
+				"year" : year
+        };
+        $.ajax({
+                data:  parametros,
+                url:   'gestionacalendario.php',
+                type:  'post',
+                beforeSend: function () {
+                },
+                success:  function (response) {
+                    var obj = JSON.parse(response);
+                    $.each(obj, function(key, value) {                        
+                        var mid='#'+value['fecha'];                        
+                        $(mid).append('<span class="glyphicon glyphicon-pushpin red"></span>&nbsp;<span class="red">'+value['titulo']+'</span></br>');
+                        
+                   });
+
+                },
+				error: function() {
+          			alert('Error occurs2!');
+       			}
+
+        });	
+}
+
+function Cargar_modal(miid)
+{    
+    var funcion='recuperamodal';       
+	
+	var parametros = {
+                "select" : funcion,				
+				"miid" : miid
+        };
+        $.ajax({
+                data:  parametros,
+                url:   'gestionacalendario.php',
+                type:  'post',
+                beforeSend: function () {
+                },
+                success:  function (response) {
+                    var visible=true;
+                    var obj = JSON.parse(response);
+                    $('#myModal .modal-body').empty(); 
+                    $.each(obj, function(key, value) {      
+                        //alert(value);
+                        if (value=="error"){
+                            visible=false;
+                        } 
+                        else
+                        {
+                            $('#myModal .modal-body').append(value['titulo']);                         
+                            visible=true;
+                        }
+                        //alert(value['titulo']);                                                                       
+                        //var mid='#'+value['fecha'];                        
+                        //$(mid).append('<span class="glyphicon glyphicon-pushpin red"></span>&nbsp;<span class="red">'+value['titulo']+'</span></br>');
+                        
+                   });
+                   if(visible)
+                   {
+                   $('#myModal').modal('show');
+                   }
+
+                },
+				error: function() {
+          			alert('Error occurs2!');
+       			}
+
+        });	
 }
 
